125f57504d0b98c05f34ec2ab0b8bc0383e8e460
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-08 17:20:21 -0300

    chore: Add application layer for product management

---
+++ application/product.go
@@ -0,0 +1,67 @@
+/**
+ * file: application/product.go
+ * description: file responsible for the application layer of the application.
+ * data: 08/01/2024
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package application
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+// constants
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Name == "" {
+		return false, nil
+	}
+
+	if p.Price < 0 {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	return nil
+}
+
+func (p *Product) Disable() error {
+	return nil
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
