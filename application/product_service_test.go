562f06ae248ba78d52abdc8d6376ae3e9c351677
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-14 13:20:09 -0300

    chore: Add github.com/golang/mock v1.6.0 dependency and update ProductService test

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -7,3 +7,28 @@
 
 package application_test
 
+import (
+	"testing"
+
+	"github.com/glaucia86/fc-studies-hexagonal-architecture-go/application"
+	mock_application "github.com/glaucia86/fc-studies-hexagonal-architecture-go/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	persistence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/golang/mock v1.6.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.8.4
 )
