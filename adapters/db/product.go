3f9ff07d8552d8ea8d3418f83abe068336d21c98
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-16 12:42:19 -0300

    chore: Add NewProductDb function to ProductDb struct and write unit test for Get method

--- adapters/db/product.go
+++ adapters/db/product.go
@@ -18,6 +18,10 @@ type ProductDb struct {
 	db *sql.DB
 }
 
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db: db}
+}
+
 func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
 	var product application.Product
 	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id = ?")
--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -1,15 +1,19 @@
 /**
  * file: adapters/db/product_test.go
  * description: file responsible for the
- * data: 15/01/2024
+ * data: 16/01/2024
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
-package db
+package db_test
 
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/glaucia86/fc-studies-hexagonal-architecture-go/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -41,4 +45,17 @@ func CreateProduct(db *sql.DB) {
 	}
 
 	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	SetUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+
+	product, err := productDb.Get("abc")
+	require.Nil(t, err, "Error when trying to get a product")
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
 }
